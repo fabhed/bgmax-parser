2a7d74fef49ecd03c79ba630e153144d5d5f6eaf
Fabian Hedin <fabian.hedin@gmail.com>
2019-04-23 15:03:44 +0200

    Everything

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-bgmax-parser
+# bgmax-parser
+
+## A Javascript Parser for Bg Max
+
+### Description
+A Parser that reads a string from a Bg Max file with Javascript.
+
+### Example Usage
+```js
+const fs = require('fs');
+const parser = require('bgmax-parser');
+
+const content = fs.readFileSync('./node_modules/bgmax-parser/example-files/bankgiroinbetalningar_exempelfil_avtal-om-ocr-kontroll_checksiffra_langd_sv.txt', 'latin1');
+const parsedBgMax = parser(content);
+console.log(parsedBgMax)
+```
+
+### Files & Folders
+
+- example-files - Contains example files downloaded from https://www.bankgirot.se/kundservice/exempelfiler/
+- index.js - The parser
+- test.js - Example usage of the parser
+
+### Links
+- https://www.bankgirot.se/en/services/incoming-payments/bankgiro-receivables/ - Information about the service "Bankgiro Receivables (Bankgiro Inbetalningar)"
+- https://www.bankgirot.se/globalassets/dokument/tekniska-manualer/bankgiroinbetalningar_tekniskmanual_sv.pdf - Technical Manual (SV) for "Bankgiro inbetalningar". Everything used to create this parser is detailed in this document.
+- https://www.bankgirot.se/globalassets/dokument/tekniska-manualer/bankgiroreceivables_bankgiroinbetalningar_technicalmanual_en.pdf - Translation to English of the Technical Manual. This was not used when creating this parser but should contain the same information.
---
+++ index.js
@@ -0,0 +1,127 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+const { entryDefinitions } = require('./config');
+
+// Extending String prototype with left and right trim methods
+String.prototype.rtrim = function (s) {
+  if (s === undefined) { s = '\\s'; }
+  return this.replace(new RegExp(`[${s}]*$`), '');
+};
+String.prototype.ltrim = function (s) {
+  if (s === undefined) { s = '\\s'; }
+  return this.replace(new RegExp(`^[${s}]*`), '');
+};
+
+// Line parser function
+function parseLine(line, lineNumber, entryObjForFormatting) {
+  // Get the transaction code (always the first two characters of each line)
+  const transactionCode = line.substring(0, 2);
+
+  // Find the correct entryDefinition
+  const entryDefinition = entryDefinitions
+    .find(e => parseInt(e.transactionCode, 10) === parseInt(transactionCode, 10));
+
+  // If no entryDefinition is find, skip this iteration by returning
+  if (!entryDefinition) return;
+
+  // Init the entry object
+  const entryObj = { transactionCode, entryDefinition, properties: [] };
+
+  let entryToFormat;
+
+  // If the function should format 'reference' properties using a referenceCode
+  let referenceCode;
+  if (entryObjForFormatting) {
+    referenceCode = entryObjForFormatting.properties.find(p => p.name === 'referenceCode').trimmedValue;
+  }
+
+  Object.keys(entryDefinition.properties).forEach((pName) => {
+    let contentFormat; let paddingFormat;
+    const property = entryDefinition.properties[pName];
+    const rawValue = line.substring(property.startPos - 1, property.endPos);
+
+    if (referenceCode && property.formatMap) {
+      property.format = property.formatMap[referenceCode];
+    }
+
+    if (property.format) {
+      let formatToUse;
+      const possibleFormats = property.format.split('/');
+
+      // The only property using multiple has the format string: 'N:h0/A:b',
+      // where A:b is to be used if the value is only spaces
+      if (possibleFormats.length === 1) [formatToUse] = possibleFormats;
+      else if (rawValue.trim()) {
+        [formatToUse] = possibleFormats;
+      } else {
+        [, formatToUse] = possibleFormats;
+      }
+      [contentFormat, paddingFormat] = formatToUse.split(':');
+    } else if (property.formatMap) {
+      entryToFormat = { lineNumber, transactionCode };
+    }
+    let trimmedValue;
+    switch (paddingFormat) {
+      // Högerställt och nollutfyllt
+      case 'h0':
+        trimmedValue = rawValue.ltrim('0');
+        break;
+      case 'vb':
+        trimmedValue = rawValue.rtrim(' ');
+        break;
+      case 'hb':
+        trimmedValue = rawValue.ltrim(' ');
+        break;
+      case 'b':
+        trimmedValue = rawValue.trim();
+        break;
+      // No format specified
+      default:
+        trimmedValue = rawValue;
+        break;
+    }
+    let formattedValue;
+    switch (contentFormat) {
+      // Numerical content
+      case 'N':
+        formattedValue = parseInt(trimmedValue, 10);
+        break;
+      // Alphanumerical content
+      case 'A':
+        formattedValue = trimmedValue;
+        break;
+      default:
+        formattedValue = trimmedValue;
+    }
+    entryObj.properties.push({
+      name: pName, trimmedValue, rawValue, value: formattedValue, contentFormat, paddingFormat,
+    });
+  });
+  // eslint-disable-next-line consistent-return
+  return { entryObj, entryToFormat };
+}
+
+// The main parser function
+module.exports = function (bgMaxStr) {
+  const parsedEntries = [];
+  const allLines = bgMaxStr.split(/\r\n|\n|\r/); // Split every line
+  const entriesToFormat = [];
+
+  let lineNumber = 0;
+  allLines.forEach((line) => {
+    if (lineNumber > 10) return;
+    const { entryObj, entryToFormat } = parseLine(line, lineNumber);
+    if (entryToFormat) {
+      entriesToFormat.push({ entryToFormat, entryObj });
+    } else {
+      parsedEntries.push(entryObj);
+    }
+    lineNumber += 1;
+  });
+  entriesToFormat.forEach(({ entryToFormat, entryObj }) => {
+    const line = allLines[entryToFormat.lineNumber];
+    const parsedLine = parseLine(line, entryToFormat.lineNumber, entryObj);
+    parsedEntries.push(parsedLine.entryObj);
+  });
+  return parsedEntries;
+};
